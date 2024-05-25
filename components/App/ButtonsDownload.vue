ee851a98c8a51ee8a021269d47a38335c43ee4c5
Artur Medeiros <arjos.eu@outlook.com>
2024-05-25 17:20:25 +0100

    Ajustes CV

--- components/App/ButtonsDownload.vue
+++ components/App/ButtonsDownload.vue
@@ -9,14 +9,14 @@ export default defineComponent({
 <template>
   <div class="pt-1 flex justify-center">
     <NuxtLink
-        to="https://arturmedeiros.pages.dev/curriculo/CV_Artur_2024.pdf"
+        to="https://arturmedeiros.pages.dev/curriculo/2024_CV_Developer_PT.pdf"
         target="_blank"
         external
     >
       <HackerButton class="pb-0" label='Currículo 🇵🇹' ></HackerButton>
     </NuxtLink>
     <NuxtLink
-        to="https://arturmedeiros.pages.dev/curriculo/CV_BR_Artur_2024.pdf"
+        to="https://arturmedeiros.pages.dev/curriculo/2024_CV_Developer_BR.pdf"
         target="_blank"
         external
     >
